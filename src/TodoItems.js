3396e96f635bd926b26f2e09d2912ea760845f61
Ernest Irabor <irabeny89@gmail.com>
2020-11-29 04:34:12 +0100

    Refactoring

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 export default function TodoItems({ entries, deletion }) {
   let createTasks = item => 
-    <li onClick={ () => deletion(item.key) } 
-      key={item.key}>{ item.text }</li>
+    <li onClick={ () => deletion(item.key) } key={item.key}>{ item.text }</li>
 
   const listItems = entries.map(createTasks)
   
